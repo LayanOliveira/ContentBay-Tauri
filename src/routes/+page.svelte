1f7d1f5215fb8b512b3d7605528d28e615c16bcf
LayanOliveira <layan3210@gmail.com>
2023-02-25 00:04:32 -0300

    My First Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Greet from '../lib/Greet.svelte';
+</script>
+
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1>Welcome to SvelteKit</h1>
+<Greet />
+
+<style lang="postcss">
+	:global(html) {
+		background-color: #3f3f3f;
+	}
+</style>
