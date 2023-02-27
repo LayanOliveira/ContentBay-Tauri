be110050afdfeb6bf3712c00e64a1f1c5f33ede7
LayanOliveira <layan3210@gmail.com>
2023-02-27 02:15:44 -0300

    Read File still not working

--- src/routes/comics/+page.svelte
+++ src/routes/comics/+page.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
 	import { open } from '@tauri-apps/api/dialog';
-    import { readTextFile, BaseDirectory } from '@tauri-apps/api/fs';
+	import { readTextFile, BaseDirectory } from '@tauri-apps/api/fs';
 
-    // const contents = ref("")
+	let contents = [''];
 
 	const readFileContents = async () => {
 		try {
 			const selectedPath = await open({
-				multiple: false, 
+				multiple: true,
 				// directory: true,
 				title: 'Open Text File',
+				filters: [
+					{
+						name: 'Image',
+						extensions: ['png', 'jpeg', 'webp', 'svg', 'cbz', 'cbr', 'pdf']
+					}
+				]
 			});
-            // contents.value = await readTextFile(selectedPath as string)
-            console.log(selectedPath)
+			// contents.value = await readTextFile(selectedPath as string);
+			console.log(selectedPath);
 		} catch (err) {
 			console.error(err);
 		}
@@ -20,19 +26,13 @@
 </script>
 
 <body class="h-screen w-screen bg-gradient-to-b from-[#141414] to-[#000000]">
-    
 	<h1 class="flex font-semibold justify-center">Welcome to Comics</h1>
 	<div class="flex justify-center p-10">
-        <!-- {#if contents.length}<p>{{contents}}</p> -->
-		<button
-			class="object-contain rounded bg-[#97181c] hover:bg-[#d81f26] p-1"
-			on:click={readFileContents}>Open File Explorer</button>
-        <!-- {/if} -->
-    </div>
+		{#if Array.isArray(contents)}<p>{{ contents }}</p>
+			<button
+				class="object-contain rounded bg-[#97181c] hover:bg-[#d81f26] p-1"
+				on:click={readFileContents}>Open File Explorer</button
+			>
+		{/if}
+	</div>
 </body>
-
-<!-- <style lang="postcss">
-	:global(html) {
-		background-color: #3f3f3f;
-	}
-</style> -->
